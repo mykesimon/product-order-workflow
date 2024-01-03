7932470e332886b4cfb5f3aed638eb2342e2efaa
Myke Simon <hi@mykesimon.com>
2024-01-03 21:06:50 +0000

    feat: add react-query and context provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+import App from './App';
+import { AppProvider } from './context/AppContext';
+
+import './main.css';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<QueryClientProvider client={queryClient}>
+			<AppProvider>
+				<App />
+			</AppProvider>
+		</QueryClientProvider>
+	</React.StrictMode>
+);
