c3900cbaa622fa07b6a291d106a85d80b484014d
Myke Simon <hi@mykesimon.com>
2024-01-03 23:05:33 +0000

    chore: set up vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,6 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -12,5 +15,11 @@ export default defineConfig({
 				secure: false,
 			},
 		},
+	},
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		css: true,
+		setupFiles: ['./src/test/setup.ts'],
 	},
 })
